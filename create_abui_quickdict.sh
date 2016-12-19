8fce328b3b82b72f276e5d9910f9c4586d24fc75
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2016-12-19 14:27:39 +0100

    Add files for creating Abui quickdict

--- create_abui_quickdict.sh
+++ create_abui_quickdict.sh
@@ -1,1 +1,1 @@
-../DictionaryPC/run.sh --lang1=ind --dictOut=abui.quickdic --dictInfo=@abui-dictinfo.txt --lang2=abz --print=printabui --input0=abui.txt --input0Name=Kratochvil Corpus --input0Format=chemnitz
+../DictionaryPC/run.sh --lang1=ind --dictOut=abui.quickdic --dictInfo=@abui-dictinfo.txt --lang2=abz --print=printabui --input0=Abui.txt --input0Name=Kratochvil Corpus --input0Format=chemnitz
